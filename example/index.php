d1f8c5c8b89604ccd5c19a33cf2061e7f60bbe24
Elger van Boxtel <elgervb@gmail.com>
2017-05-05 05:49:35 +0200

    add validation and examples

--- example/index.php
+++ example/index.php
@@ -1,17 +1,29 @@
 <?php
 require __DIR__ . '/../vendor/autoload.php';
 
+ini_set('html_errors', 'off');
+
 use app\App;
+use validate\Args;
+use handler\json\Json;
+use handler\json\JsonHandler;
+use sysinfo\Sysinfo;
 
 /**
  *
  * @author Elger van Boxtel
  *
  */
-$app = new App();
+$app = App::create();
+$app->addHandler(new JsonHandler());
 
-$app->route('/info', function() {
-	phpinfo();
+$app->route('/info/?([0-9]+)?', function($what = null) {
+	$what = Args::int($what)->value();
+	$what ? phpinfo($what) : phpinfo();
+});
+
+$app->route('/sysinfo', function() {
+	return new Json(Sysinfo::create());
 });
 
 $app->start();
